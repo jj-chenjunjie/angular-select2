2149da9d4b10451b2c78542129ddbbc123710819
Jason <943388837@qq.com>
2016-07-06 17:20:25 +0800

    update

--- angular-select2.js
+++ angular-select2.js
@@ -1,82 +1,59 @@
-(function(module){	
+(function () {
+	var ANGULAR_SELECT_OPTION_REGEXP = new RegExp(/^\?\s\S*\s\?$/);
+	
+	var mSelect2 = angular.module('select2', []);
+	
+	mSelect2.directive('select2', function () {
 
-})(function(angular){
-	return angular.module('ui', []).directive('angularSelect2', function($http){
 		return {
 			scope: {
-				angularSelect2: '=',
+				select2: '=',
 				ngModel: '='
-			},
-			restrict: 'A',
-			link: {
-				pre: function(scope, element, attrs, controllers) {
-					
-//					console.log("pre");
-					var select2, data;
-					
-					if(scope.angularSelect2 instanceof Array){
-						// 传递数组
-						data = scope.angularSelect2;
-					}else if(scope.angularSelect2 instanceof Object){
-						// 传递select2配置选项
-						data = scope.angularSelect2.data;
-					}else {
-						// 传递url
-						$http.get(attrs.url).success(function(res){
-							data = res;
-							init();
-						});
-					}
-
-					scope.$watch('angularSelect2', function(newValue, oldValue, scope) {
-						if(newValue){
-							init();
-						};
-					});
-					
-					// 监听Model的变化
-					scope.$watch('ngModel', function(newValue, oldValue, scope) {
-						console.log('ngModel', newValue, oldValue)
-						// 当model为时，设置默认值
-						if (!scope.ngModel) {
-							// 是否允许清空选项
-							if(!attrs.allowClear){
-								scope.ngModel = data && data[0] && data[0].id;	
-							}
-						}
-						var firstOption = element.children().first();
-//						console.log(firstOption.val());
-						if(firstOption && firstOption.val() && firstOption.val().indexOf('?') >= 0){
-							element.children().first().remove();
-						}
-						select2 && select2.val(scope.ngModel).trigger('change.select2');
-						
-					});
-					
-					/**
-					 * 初始化select2
-					 */
-					function init() {
-						element.html(attrs.allowClear ? '<option></option>' : '');
-						if (scope.angularSelect2 instanceof Array) {
-							select2 = element.select2({
-								data: scope.angularSelect2
-							});
-						} else if(scope.angularSelect2 instanceof Object){
-							select2 = element.select2(scope.angularSelect2);
-						}else {
-							select2 = element.select2({
-								data: data
-							});
-						}
-						select2.val(scope.ngModel).trigger('change.select2');
-					}
+			}, // {} = isolate, true = child, false/undefined = no change
+			restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+			link: function($scope, iElm, iAttrs, controller) {
 				
-				},
-				post: function(scope, element, attrs, controllers) {
+				var $select2;	// select2 jquery 对象
+				init();
+				// 监听Model的变化
+				$scope.$watch('ngModel', function(newValue) {
+//					console.log('ngModel', newValue);
+					// angular 重新定义了select指令，会为select添加一个option，这里删除它
+					if(ANGULAR_SELECT_OPTION_REGEXP.test(iElm.children().first().val())) {
+						iElm .children().first().remove();						
+					}
+					// 若有model有值，更新select2
+					if (newValue) {
+						$select2 && $select2.val(newValue).trigger('change.select2');
+					}
+				});
+				
+				
+				// 监听Model的变化, 重新实例化
+				$scope.$watch('select2', function() {
+					init();
+				});
+
+				/**
+				 * 初始化select2
+				 */
+				function init() {
+					// 当有allow-clear属性时，需要手动为select添加一个占位的option
+					iElm.html(iAttrs.allowClear ? '<option value=""></option>' : '');
+					$select2 = iElm.select2($scope.select2);	
+					if (!iAttrs.allowClear) {
+						try{
+							$scope.ngModel = $scope.ngModel || $scope.select2.data[0].id;	
+						}catch(e){
+							
+						}
+						 
+					}
 					
 				}
-		    },
-		};
-	});
-}(angular));
+				
+			}
+			
+		}
+	})
+})();
